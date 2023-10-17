835b2b7814f4086ad529f42f8e120952b033ae2d
Janak singh dhami <info@jsdhami.com.np>
2023-10-17 22:38:49 +0545

    update nav bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>HELLO JANAK I'M FROM CHITWAN WHAT ARE YOU DOING?</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
